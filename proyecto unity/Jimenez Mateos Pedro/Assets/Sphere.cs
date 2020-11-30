c95969faecb085cc3c766dc7e2afe90ac251c710
Pedro Jimenez Mateos <72516362+pedrojm11@users.noreply.github.com>
2020-11-30 17:44:24 +0100

    interactividad y restriccion
    he consegido que la bola se mueva correctamente  y he restringido el movimiento en el eje  x

---
+++ proyecto unity/Jimenez Mateos Pedro/Assets/Sphere.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float myspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myspeed=2.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Moverbola();
+    }
+    void Moverbola()
+    {
+        float desplZ = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * myspeed * desplZ);
+        float desplX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * myspeed * desplX);
+        float PosX = transform.position.x;
+        float PosZ = transform.position.z;
+        if (PosX > -4.8 && PosX < 4.8)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * myspeed * desplX);
+        }
+        else if (PosX < -4 && desplX > 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * myspeed * desplX);
+        }
+        else if (PosX > 4 && desplX < -0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * myspeed * desplX);
+        }
+
+
+
+    }
+    
+}
+
+
+   
+
