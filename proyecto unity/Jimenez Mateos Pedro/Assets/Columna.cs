89148f27b342f1c3e12bcd17622fc9df1524657c
Pedro Jimenez Mateos <72516362+pedrojm11@users.noreply.github.com>
2020-11-30 19:29:24 +0100

    Columnas aleatorias
    Creo que he creado mal el prefab de las columnas y por eso no sale pero he intentado escribirte el codigo

---
+++ proyecto unity/Jimenez Mateos Pedro/Assets/Columna.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Columna : MonoBehaviour
+{
+    public float mySpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void MovimientoColumna()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * mySpeed);
+        if (transform.position.y > 25)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
---
+++ proyecto unity/Jimenez Mateos Pedro/Assets/crearcolumnas.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crearcolumnas : MonoBehaviour
+{
+    [SerializeField] Transform Cylinder;
+    [SerializeField] Transform RefPos;
+    [SerializeField] float distobstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ColumnCorrutine");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CrearColumna();
+        }
+    }
+    void CrearColumna()
+    {
+       
+        float posRandom = Random.Range(0f, 30f);
+       
+        Vector3 DestPos = new Vector3(0, 0, posRandom);
+       
+        Vector3 NewPos = RefPos.position + DestPos;
+        
+        Instantiate(Cylinder, NewPos, Quaternion.identity);
+    }
+    
+    IEnumerator ColumnCorrutine()
+    {
+        
+        for (int n = 0; ; n++)
+        {
+            
+            print(n);
+            
+            
+            CrearColumna();
+            
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+ 
+}
