87e7e193f7002a4581d4a4f9b47b4722fbf74bca
Pedro Jimenez Mateos <72516362+pedrojm11@users.noreply.github.com>
2020-11-30 18:47:35 +0100

    Camara
    he conseguido que la camara acompañe a la esfera

---
+++ proyecto unity/Jimenez Mateos Pedro/Assets/Camera1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera1 : MonoBehaviour
+{
+    [SerializeField] Transform GameObject;
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(GameObject.position.x, transform.position.y, GameObject.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+    }
+}
